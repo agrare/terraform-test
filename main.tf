dd40b159b36003c24f91e77002a403cbf5f8a7c7
Adam Grare <adam@grare.com>
2025-07-17 13:10:50 -0400

    Replace with AWS provision

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "stack_name" {
+  type    = string
+  default = "awssamplevm1"
+}
+variable "public_ssh_key_name" {
+  description = "Name of the public SSH key used to connect to the virtual guest"
+  default     = "bptest00527052401"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "bp_sample_template" {
+  ami           = "ami-03e9149278a6f457c"
+  instance_type = "t2.nano"
+  key_name      = var.public_ssh_key_name
+
+  tags = {
+    Name = var.stack_name
+  }
+}
